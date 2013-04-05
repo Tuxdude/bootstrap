3c69d1c3e18c5917c97a63970e57b68a61bdf6d3
Ash <tuxdude@StarScream>
2013-04-05 00:41:00 -0700

    Add various python bin dirs into PATH.

---
+++ .bashrc-plugins/045-python.sh
@@ -0,0 +1,9 @@
+#
+# Author: Ash <tuxdude.github@gmail.com>
+#
+
+PYTHON_PATHS="/opt/python/python3.3 /opt/python/python3.2 /opt/python/python3.1 /opt/python/python2.7 /opt/python/python2.6"
+
+for path in $PYTHON_PATHS; do
+    export PATH=$(path_remove "$PATH" "$path/bin"):$path/bin
+done
