8e8384b49da600aa8f7ffb48992f059c04cc4f57
Ash <tuxdude@WingSaber>
2013-04-09 22:11:03 -0700

    Add some perforce aliases and helper functions.

---
+++ .bashrc-plugins/035-p4.sh
@@ -0,0 +1,14 @@
+#
+# Author: Ash <tuxdude.github@gmail.com>
+#
+
+# List all opened files in the current directory showing relative path names
+# in the output
+p4openedlist() {
+    p4 opened ... | sed 's/#.*//' | xargs p4 where | sed 's/.* //' | cut -d '/' -f $(($(echo $PWD | sed s'/\// /g' | wc -w)+2))-
+}
+
+# p4 aliases
+alias p4clidiff='P4DIFF="colordiff -u" p4 diff'
+alias p4vimdiff='P4DIFF="vimdiff -f" p4 diff'
+alias p4gvimdiff='P4DIFF="gvimdiff -f" p4 diff'
