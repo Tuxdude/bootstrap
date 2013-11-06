1fccae8145d3d86fc1abade0b9d8ce49dea77ee7
Ash <tuxdude@WingSaber>
2013-11-06 14:31:48 -0800

    Always enable bash completion (Ubuntu requires this).

---
+++ .bashrc-plugins/039-completion.sh
@@ -0,0 +1,8 @@
+#
+# Author: Ash <tuxdude.github@gmail.com>
+#
+
+# Enable bash completion (Ubuntu requires this)
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    source /etc/bash_completion
+fi
