5518199f9fa4e9e8670f929a11738107924319a9
Ash <tuxdude@WingSaber>
2013-11-06 14:30:57 -0800

    Adding some shell tweaks.

---
+++ .bashrc-plugins/005-shell.sh
@@ -0,0 +1,12 @@
+#
+# Author: Ash <tuxdude.github@gmail.com>
+#
+
+# Append to history
+shopt -s histappend
+
+HISTSIZE=10000
+HISTFILESIZE=20000
+
+# Update LINES and COLUMNS to reflect the window size after each command
+shopt -s checkwinsize
