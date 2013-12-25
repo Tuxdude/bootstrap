b0a1066660d64517bd0691244e3092bd2bb2a608
Ash <tuxdude@OptimusPrime>
2013-12-24 20:08:42 -0800

    Check for presence of brew.
    On non-mac systems, and those which do not have /etc/bash_completion,
    the elif would fail since brew does not exist.
    
    Check for existence of brew before invoking it.

--- .bashrc-plugins/039-completion.sh
+++ .bashrc-plugins/039-completion.sh
@@ -6,6 +6,6 @@
 if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     source /etc/bash_completion
 # OS X has bash completion installed in a different location
-elif [ -f $(brew --prefix)/etc/bash_completion ]; then
+elif hash brew 2>/dev/null && [ -f $(brew --prefix)/etc/bash_completion ]; then
     source $(brew --prefix)/etc/bash_completion
 fi
