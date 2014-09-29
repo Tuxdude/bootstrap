32e5893355bb4ea092ddb664dc43c6d7b1278f0d
Ash <tuxdude@WingSaber>
2014-09-29 14:32:58 -0700

    Enable direxpand shopt for bash >= 4.2.25.

--- .bashrc-plugins/005-shell.sh
+++ .bashrc-plugins/005-shell.sh
@@ -10,3 +10,10 @@ HISTFILESIZE=20000
 
 # Update LINES and COLUMNS to reflect the window size after each command
 shopt -s checkwinsize
+
+# Enable directory expansion for variables
+# Example : 'ls $MY_DIR <Tab>' will expand MY_DIR
+if ((BASH_VERSINFO[0] == 4)) && ((BASH_VERSINFO[1] >= 2 )) && ((BASH_VERSINFO[2] >= 20)) || \
+   ((BASH_VERSINFO[0] >= 5)); then
+    shopt -s direxpand
+fi
