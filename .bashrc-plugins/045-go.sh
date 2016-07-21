813bdf1d188490216163fcc5b10fda072c11cc6f
Ash <tuxdude@OptimusPrime>
2016-07-20 22:59:07 -0700

    Update GOROOT env variable.

--- .bashrc-plugins/045-go.sh
+++ .bashrc-plugins/045-go.sh
@@ -2,6 +2,6 @@
 # Author: Ash <tuxdude.io@gmail.com>
 #
 
-export GOPATH="$HOME/.local/go"
-gopath_bin="$GOPATH/bin"
+export GOROOT="$HOME/.local/go"
+gopath_bin="$GOROOT/bin"
 export PATH=$gopath_bin:$(path_remove "$PATH" "$gopath_bin")
