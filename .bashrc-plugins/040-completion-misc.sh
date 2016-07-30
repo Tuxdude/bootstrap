7906dfb21b54f757e099634cd996e2aedc203df9
Ash <tuxdude@StarScream>
2016-07-30 00:35:33 -0700

    Make pip bash completion on explicit demand.
    This saves a lot of shell startup time.

--- .bashrc-plugins/040-completion-misc.sh
+++ .bashrc-plugins/040-completion-misc.sh
@@ -7,10 +7,14 @@ _git_forest () {
     _git_log
 }
 
-# pip bash completion
-if hash pip 2> /dev/null; then
-    eval "$(pip completion --bash)"
-fi
+# pip bash completion (increases start up time)
+# So define a function to load pip completion manually
+load_pip_completion() {
+    unset -f load_pip_completion
+    if hash pip 2> /dev/null; then
+        eval "$(pip completion --bash)"
+    fi
+}
 
 # aws bash completion
 if hash aws_completer 2> /dev/null; then
