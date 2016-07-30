85508d9624276a6f8d48b4f837db12b1bc164431
Ash <tuxdude@StarScream>
2016-07-30 00:06:55 -0700

    Lazy initialize nvm.
    - This helps us cut down shell startup initialization drastically
    although will add the cost to the first time execution of either nvm or
    some node based binary (including node itself)

--- .bashrc-plugins/065-nodejs-iojs.sh
+++ .bashrc-plugins/065-nodejs-iojs.sh
@@ -7,10 +7,31 @@
 if hash brew 2> /dev/null; then
     # Explicitly set the NVM_DIR to avoid confusing nvm
     export NVM_DIR="$(readlink -f $(brew --prefix nvm))"
-    # Bootstrap nvm
-    source "$(brew --prefix nvm)/nvm.sh"
+
+    # BEGIN UGLY HACK
+    # This lets us initialize nvm, node and any other global node binaries on demand (lazily)
+    # to save on shell startup time
+    declare -a NODE_GLOBALS=(`find $NVM_DIR/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+
+    NODE_GLOBALS+=("node")
+    NODE_GLOBALS+=("nvm")
+
+    # Bootstrap nvm on demand
+    load_nvm() {
+        source "$NVM_DIR/nvm.sh"
+
+        # Set the version of node to use from ~/.nvmrc if available
+        nvm use 2> /dev/null 1>&2 || true
+
+        # Do not reload nvm again
+        export NVM_LOADED=1
+    }
+
+    for node_cmd in "${NODE_GLOBALS[@]}"; do
+        eval "${node_cmd}() { unset -f ${node_cmd}; [ -z \${NVM_LOADED+x} ] && load_nvm; ${node_cmd} \$@; }"
+    done
+    # END UGLY HACK
+
     # Bash completion for nvm
     source $NVM_DIR/etc/bash_completion.d/nvm
-    # Set the version of node to use from ~/.nvmrc if available
-    nvm use 2> /dev/null 1>&2 || true
 fi
