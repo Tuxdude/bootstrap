50d0051355b9a02e7ba40ba1709ac2beace35a2b
Ash <tuxdude@StarScream>
2016-08-14 17:34:15 -0700

    Request JDK 1.8 by default.

--- .bashrc-plugins/055-java-groovy.sh
+++ .bashrc-plugins/055-java-groovy.sh
@@ -6,7 +6,7 @@
 # Use the java_home utility to set JAVA_HOME automatically
 
 if [[ $OSTYPE == darwin* ]]; then
-    REQUESTED_JAVA_VERSION="1.7"
+    REQUESTED_JAVA_VERSION="1.8"
     FALLBACK_JAVA_HOME="$HOME/jdk1.7.0_80.jdk/Contents/Home"
     if POSSIBLE_JAVA_HOME="$(/usr/libexec/java_home -v $REQUESTED_JAVA_VERSION 2>/dev/null)"; then
         export JAVA_HOME="$POSSIBLE_JAVA_HOME"
