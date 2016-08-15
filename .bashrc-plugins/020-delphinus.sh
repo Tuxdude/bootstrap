0751bf6a66d929eb669152ff967c03948158948a
Ash <tuxdude@StarScream>
2016-08-14 17:38:52 -0700

    Disable deprecated Delphinus support.

--- .bashrc-plugins/020-delphinus.sh
+++ .bashrc-plugins/020-delphinus.sh
@@ -2,11 +2,12 @@
 # Author: Ash <tuxdude.io@gmail.com>
 #
 
-DELPHINUS_HOME="$HOME/git-repos/delphinus"
-DELPHINUS_BIN="$DELPHINUS_HOME/dist/host-gcc/bin"
-DELPHINUS_LIBS="$DELPHINUS_HOME/dist/host-gcc/lib"
-export LD_LIBRARY_PATH=$(path_remove "$LD_LIBRARY_PATH" "$DELPHINUS_LIBS"):$DELPHINUS_LIBS
-export PATH=$(path_remove "$PATH" "$DELPHINUS_BIN"):$DELPHINUS_BIN
-unset DELPHINUS_HOME DELPHINUS_BIN DELPHINUS_LIBS
-
-alias clocdelphinus='cloc --by-file-by-lang --force-lang="make",mk .'
+# Disable Delphinus support, since we don't use it any more.
+# DELPHINUS_HOME="$HOME/git-repos/delphinus"
+# DELPHINUS_BIN="$DELPHINUS_HOME/dist/host-gcc/bin"
+# DELPHINUS_LIBS="$DELPHINUS_HOME/dist/host-gcc/lib"
+# export LD_LIBRARY_PATH=$(path_remove "$LD_LIBRARY_PATH" "$DELPHINUS_LIBS"):$DELPHINUS_LIBS
+# export PATH=$(path_remove "$PATH" "$DELPHINUS_BIN"):$DELPHINUS_BIN
+# unset DELPHINUS_HOME DELPHINUS_BIN DELPHINUS_LIBS
+# 
+# alias clocdelphinus='cloc --by-file-by-lang --force-lang="make",mk .'
