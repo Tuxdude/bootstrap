955593cec46e6065404acb2487d975fb1465b379
Ash <tuxdude@Baymax.area51>
2022-01-30 19:26:14 -0800

    Add $GOPATH/bin to $PATh for easier access to the installed binaries.

--- .bashrc-plugins/045-go.sh
+++ .bashrc-plugins/045-go.sh
@@ -5,3 +5,5 @@
 export GOROOT="$HOME/.local/go"
 goroot_bin="$GOROOT/bin"
 export PATH=$goroot_bin:$(path_remove "$PATH" "$goroot_bin")
+gopath_bin="$HOME/go/bin"
+export PATH=$gopath_bin:$(path_remove "$PATH" "$gopath_bin")
