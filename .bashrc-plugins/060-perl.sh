7d24a20f448d1b901b305ddf428b3efecd8a38e7
Ash <tuxdude@IronHide>
2024-05-27 16:42:06 -0700

    Include the correct location of Git.pm on osx.

--- .bashrc-plugins/060-perl.sh
+++ .bashrc-plugins/060-perl.sh
@@ -6,5 +6,8 @@
 if [[ $OSTYPE == darwin* ]]; then
     if hash brew 2>/dev/null; then
         export PERL5LIB="$(brew --prefix)/lib/perl5/site_perl"
+        if brew ls --versions git >/dev/null; then
+            export PERL5LIB="$PERL5LIB:$(dirname $(brew list git | grep 'Git.pm'))"
+        fi
     fi
 fi
